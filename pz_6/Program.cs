e50dc69ac53e88e5f300f7cc03423c38ccdc4fe8
Черников Александр <alexonbaton@gmail.com>
2022-09-26 13:32:51 +0500

    Обязательно

---
+++ pz_6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 1;
+            Console.WriteLine("Число " + "квадрат " + "куб");
+            while (0 < x && x < 11)
+            {
+                Console.WriteLine(x + " " + Math.Pow(x, 2) + " " + Math.Pow(x, 3));
+                x++;
+            }
+            
+            int y = 0;
+            Console.WriteLine("Число " + "квадрат " + "куб");
+            do
+            {
+                y++;
+                Console.WriteLine(y + " " + Math.Pow(y, 2) + " " + Math.Pow(y, 3));
+
+            }
+            while (y > 0 && y < 10);
+               
+
+        }
+    }
+}
