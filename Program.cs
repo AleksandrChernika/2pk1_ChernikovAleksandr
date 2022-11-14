b1ec1c0c086d8b314ad12a93ed804f1b04a88bbd
AleksandrChernika <114126850+AleksandrChernika@users.noreply.github.com>
2022-11-14 12:08:09 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
-﻿using System.Linq.Expressions;
-
-namespace praktika_12 //Практика 12
+﻿namespace pz_13
 {
     internal class Program
     {
-            static void TrianglePS(double a, out double P, out double S)
+        static IEnumerable<string> Method(string[] mas)
+        {
+            for (int i = 0; i < mas[4].Length; i++)
             {
-                P = 3 * a;
-                S = Math.Sqrt(Math.Pow(a, 2.0) * 3.0 / 4.0);
+                mas[i] = mas[i].PadRight(mas[4].Length, '*');
+                Console.WriteLine(mas[i]);
             }
+            return mas;
         }
 
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
-            double P, S;
-            Console.WriteLine("Введите длину стороны равностороннего треугольника: ");
-            double a = double.Parse(Console.ReadLine());
-            TrianglePS(a, out P, out S);
-            Console.WriteLine("Периметр = " + P);
-            Console.WriteLine("Площадь = " + S);
+            string[] mas = new string[] { "a", "bb", "ccc", "dddd", "eeeee" };
+            Method(mas);
         }
     }
 }
