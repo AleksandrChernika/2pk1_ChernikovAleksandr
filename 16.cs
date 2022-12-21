ad5db8d5e3e134c30302d30072ddf297f5f5c2a6
AleksandrChernika <114126850+AleksandrChernika@users.noreply.github.com>
2022-12-21 23:09:34 +0500

    Add files via upload

---
+++ 16.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sherganov.bb2340
+ * Date: 28.11.2022
+ * Time: 9:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace pz_16
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string path = @"D:\pz16.txt";
+			FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+			StreamReader rd = new StreamReader(file);
+			string [] mass = new string[10];
+			int[] god = new int[10];
+			int[] mes = new int[10];
+			int[] den = new int[10];
+ 			for (int i = 0; i < mass.Length; i++) 
+			{
+				string str = rd.ReadLine();
+				mass [i] = str;
+			}
+ 			for (int i = 0; i < mass.Length; i++) 
+ 			{
+ 				god[i] = int.Parse(mass[i].Remove(4));
+ 				mes[i] = int.Parse(mass[i].Substring(5, 2));
+ 				den[i] = int.Parse(mass[i].Substring(8, 2));
+ 			} 
+ 			Array.Sort(god);
+ 			Console.WriteLine(mass[8]);
+			rd.Close();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 18.cs
@@ -0,0 +1,57 @@
+﻿namespace pz_18
+{
+
+    internal class Program
+    {
+        public enum Marks: int //создание перечисления
+        {
+            Very_bad = 1,
+            Bad = 2,
+            Normal = 3,
+            Good = 4,
+            Very_good = 5
+        }
+        static void Main(string[] args)
+        {
+            try //для обработки исключений
+            {
+                int str = int.Parse(Console.ReadLine());
+                switch (str) //метод switch
+                {
+                    case (int)Marks.Very_bad:
+                        {
+                            Console.WriteLine("Очень плохо");
+                            break;
+                        }
+                    case (int)Marks.Bad:
+                        {
+                            Console.WriteLine("Плохо");
+                            break;
+                        }
+                    case (int)Marks.Normal:
+                        {
+                            Console.WriteLine("Удовлетворительно");
+                            break;
+                        }
+                    case (int)Marks.Good:
+                        {
+                            Console.WriteLine("Хорошо");
+                            break;
+                        }
+                    case (int)Marks.Very_good:
+                        {
+                            Console.WriteLine("Очень хорошо");
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}   
+
