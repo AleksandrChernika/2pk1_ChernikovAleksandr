0892cbd5c91dc62ac3dee8158d336f1ae363ff5a
AleksandrChernika <alexonbaton@gmail.com>
2023-03-18 08:15:00 +0300

    pz_21

---
+++ pz_21/pz_26.1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_26._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+    public static class Files
+    {
+        static string path = @"D:\data\";
+        static public FileInfo file;
+        static FileStream file1 = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+        static public FileInfo CreateFile()
+        {
+            string name;
+            return file;
+        }
+    }
+}
